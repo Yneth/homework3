4ec236c594a0ff38ff549a6c5a91db6a467827b9
Anthony <zaknofein2@gmail.com>
2016-11-17 23:05:54 +0200

    refactored object construction

--- src/main/java/ua/abond/generics/factory/SuppliableFactory.java
+++ src/main/java/ua/abond/generics/factory/SuppliableFactory.java
@@ -4,28 +4,33 @@ import java.lang.reflect.Field;
 import java.util.function.Supplier;
 
 public class SuppliableFactory<T> extends PhantomFactory<T> {
-    private final Supplier<T> supplier;
+    private final Supplier<? extends T> supplier;
 
-    public SuppliableFactory(int maxRefCount, Class<? extends T> type, Supplier<T> supplier) {
+    protected SuppliableFactory(int maxRefCount, Supplier<? extends T> supplier) {
         super(maxRefCount);
-        checkSupplier(type, supplier);
         this.supplier = supplier;
     }
 
-    private void checkSupplier(Class<? extends T> type, Supplier<? extends T> supplier) {
+    @Override
+    protected T newInstance() {
+        return supplier.get();
+    }
+
+    public static <T> SuppliableFactory<T> create(int maxRefCount,
+                                                  Class<? extends T> type,
+                                                  Supplier<? extends T> supplier) {
+        checkSupplier(type, supplier);
+        return new SuppliableFactory<>(maxRefCount, supplier);
+    }
+
+    private static <T> void checkSupplier(Class<? extends T> type, Supplier<? extends T> supplier) {
         Class<? extends Supplier> aClass = supplier.getClass();
         Field[] declaredFields = aClass.getDeclaredFields();
         for (Field f : declaredFields) {
             Class<?> fType = f.getType();
             if (type.isAssignableFrom(fType)) {
-                throw new IllegalArgumentException("InputStream was captured by lambda expression.");
+                throw new IllegalArgumentException("Supplied value was captured by lambda expression.");
             }
         }
-    }
-
-
-    @Override
-    protected T newInstance() {
-        return supplier.get();
     }
 }
