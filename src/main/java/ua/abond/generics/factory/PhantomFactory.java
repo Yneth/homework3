c1bc26ccbe00e6fbe811196152faf3bef7649657
Anthony <zaknofein2@gmail.com>
2016-11-17 23:05:39 +0200

    refactoring names

--- src/main/java/ua/abond/generics/factory/PhantomFactory.java
+++ src/main/java/ua/abond/generics/factory/PhantomFactory.java
@@ -2,9 +2,7 @@ package ua.abond.generics.factory;
 
 import org.apache.log4j.Logger;
 
-import java.lang.ref.PhantomReference;
-import java.lang.ref.Reference;
-import java.lang.ref.ReferenceQueue;
+import java.lang.ref.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +10,7 @@ public abstract class PhantomFactory<T> {
     private static final long TIMEOUT = 200;
     private static final Logger LOGGER = Logger.getLogger(PhantomFactory.class);
 
-    private final List<Reference<? extends T>> refs = new ArrayList<>();
+    private final List<Reference<? extends T>> references = new ArrayList<>();
 
     protected final int maxRefCount;
     protected final ReferenceQueue<T> queue;
@@ -28,7 +26,7 @@ public abstract class PhantomFactory<T> {
 
     public T create() throws InterruptedException {
         while (true) {
-            if (refs.size() < maxRefCount) {
+            if (references.size() < maxRefCount) {
                 return wrap(newInstance());
             }
 
@@ -39,7 +37,7 @@ public abstract class PhantomFactory<T> {
                     System.gc();
                     continue;
                 }
-                refs.remove(ref);
+                references.remove(ref);
                 ref.clear();
             } catch (InterruptedException e) {
                 LOGGER.error("Failed to remove reference from the queue.", e);
@@ -49,11 +47,11 @@ public abstract class PhantomFactory<T> {
     }
 
     public int liveObjectsCount() {
-        return refs.size();
+        return references.size();
     }
 
     protected T wrap(T t) {
-        refs.add(new PhantomReference<>(t, queue));
+        references.add(new WeakReference<>(t, queue));
         return t;
     }
 
